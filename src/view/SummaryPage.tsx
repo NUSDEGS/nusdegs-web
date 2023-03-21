3cb20f2025431ed5f1fbd9121b58f86e5643e89d
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-21 09:52:04 +0800

    refactor SummaryPage using Section component

--- src/view/SummaryPage.tsx
+++ src/view/SummaryPage.tsx
@@ -1,32 +1,26 @@
-import { Center, Heading, VStack, Text, HStack, Divider, Button } from "@chakra-ui/react";
+import { VStack, Text, HStack, Divider, Button } from "@chakra-ui/react";
+import Section from "./components/Section";
 
 export default function SummaryPage() {
     return (
-        <Center height='100vh' width='100vw'>
-        <VStack width='80%' spacing='2rem'>
-          <VStack width='100%' align='start' spacing='2rem'>
-            <Heading size='4xl'>All Good?</Heading>
-            <Text>Check your choices.</Text>
+      <Section title='All Good?' description='Check your choices.'>
+        <HStack>
+          <VStack align='end'>
+            <Text as='b'>Major</Text>
+            <Text as='b'>Focus Areas</Text>
           </VStack>
 
-          <HStack>
-            <VStack align='end'>
-              <Text as='b'>Major</Text>
-              <Text as='b'>Focus Areas</Text>
-            </VStack>
+          <Divider orientation='vertical' />
 
-            <Divider orientation='vertical' />
-
-            <VStack align='start'>
-              <Text>Computer Science</Text>
-              <Text>Networking, and Databases</Text>
-            </VStack>
-          </HStack>
-
-          <VStack width='100%' align='end'>
-            <Button colorScheme='green'>Looks good! Generate the plan</Button>
+          <VStack align='start'>
+            <Text>Computer Science</Text>
+            <Text>Networking, and Databases</Text>
           </VStack>
+        </HStack>
+
+        <VStack width='100%' align='end'>
+          <Button colorScheme='green'>Looks good! Generate the plan</Button>
         </VStack>
-      </Center>
+      </Section>
     )
 }
