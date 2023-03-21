7c9ffd7b17f16a4f8ab1a989cb2a05c206bb7ab8
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-21 19:47:07 +0800

    refactor MajorPage using ImageRadio component

--- src/view/MajorPage.tsx
+++ src/view/MajorPage.tsx
@@ -1,32 +1,14 @@
-import { Center, Heading, VStack } from '@chakra-ui/layout'
-import { Text, Box, Image, useRadio } from '@chakra-ui/react'
+import ImageRadio from './components/ImageRadio'
 import Section from './components/Section'
 
 export default function MajorPage() {
-  const MajorRadio = (props: any) => {
-    const {image, ...radioProps} = props
-    const { state, getInputProps, getCheckboxProps, htmlProps, getLabelProps } = useRadio(radioProps)
-
-    return (
-      <Box as='label'>
-        <VStack {...htmlProps} cursor='pointer'>
-          <input {...getInputProps({})} hidden/>
-          <Box
-            {...getCheckboxProps()}
-            boxSize='8rem'
-          >
-            <Image src='https://www.svgrepo.com/show/12668/computer.svg' {...getLabelProps()} />
-          </Box>
-
-          <Text as='b' color={state.isChecked ? 'green.500' : ''}>Computer Science</Text>
-        </VStack>
-      </Box>
-    )
-  }
-
   return (
     <Section title='Major' description='Choose your major.'>
-      <MajorRadio />
+      <ImageRadio
+        image='https://www.svgrepo.com/show/12668/computer.svg'
+        label='Computer Science'
+        value='computer science'
+      />
     </Section>
   )
 }
