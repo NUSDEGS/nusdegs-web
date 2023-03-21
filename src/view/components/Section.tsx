90d6c7aaa193a8e4615ed19657b46ef19055c1e7
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-21 09:43:40 +0800

    add alignContent prop to Section

--- src/view/components/Section.tsx
+++ src/view/components/Section.tsx
@@ -4,23 +4,27 @@ import { ReactNode } from "react"
 interface SectionProps {
   title: string
   description?: string
+  alignContent?: string
   children?: ReactNode
 }
 
 export default function Section(props: SectionProps) {
   return (
     <Center height='100vh' width='100vw'>
-    <VStack width='80%' align='start' spacing='2rem'>
-      <Heading size='4xl'>{props.title}</Heading>
+      <VStack width='100%' spacing='2rem'>
+        <VStack width='80%' align='start' spacing='2rem'>
+          <Heading size='4xl'>{props.title}</Heading>
 
-      {
-        props.description &&
-          <Text>{props.description}</Text>
-      }
+          {
+            props.description &&
+              <Text>{props.description}</Text>
+          }
+        </VStack>
 
-
-      {props.children}
-    </VStack>
-  </Center>
+        <VStack width='80%' align={props.alignContent ?? 'center'}>
+          {props.children}
+        </VStack>
+      </VStack>
+    </Center>
   )
 }
