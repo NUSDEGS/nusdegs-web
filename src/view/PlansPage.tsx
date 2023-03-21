742221bb143bd924bfe2bcd452b2a0e3e2196ec3
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-21 18:38:43 +0800

    refactor PlansPage using Plan component

--- src/view/PlansPage.tsx
+++ src/view/PlansPage.tsx
@@ -1,98 +1,53 @@
-import { Center, Heading, VStack, Text, HStack, Table, Thead, Th, Tbody, Tr, Td, Tag, TagLabel, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Flex, Spacer } from "@chakra-ui/react";
+import Plan, { Semester } from "./components/Plan";
 import Section from "./components/Section";
+
+// This const is used just for testing.
+const sems: Semester[] = [
+  {
+    seqNum: 0,
+    modules: [
+      {
+        code: 'CS1101S',
+        title: 'Programming Methodology',
+        mcs: 4,
+        tags: ['core']
+      },
+      {
+        code: 'CS2040',
+        title: 'Data Structures and Algorithms',
+        mcs: 4,
+        tags: ['core', 'algorithms']
+      },
+    ]
+  },
+  {
+    seqNum: 1,
+    modules: [
+      {
+        code: 'CS5224',
+        title: 'Cloud Computing',
+        mcs: 4,
+        tags: ['networking']
+      }
+    ]
+  },
+  {
+    seqNum: 2,
+    modules: [
+      {
+        code: 'CS5224',
+        title: 'Cloud Computing',
+        mcs: 4,
+        tags: ['networking']
+      }
+    ]
+  },
+]
 
 export default function PlansPage() {
   return (
     <Section title='Plans' description='Here are your recommended plans.'>
-      <Accordion width='100%' borderWidth='1px' allowToggle allowMultiple>
-          <AccordionItem>
-            <AccordionButton>
-              <Flex width='100%'>
-                <Heading size='md'>Year 1 Semester 1</Heading>
-                <Spacer />
-                <AccordionIcon />
-              </Flex>
-            </AccordionButton>
-
-            <AccordionPanel padding='0'>
-              <Table>
-                <Thead>
-                  <Tr>
-                    <Th>Module</Th>
-                    <Th>Credits</Th>
-                    <Th>Tags</Th>
-                  </Tr>
-                </Thead>
-
-                <Tbody>
-                  <Tr>
-                    <Td width='60%'>CS2103T Software Engineering</Td>
-                    <Td width='10%'>4</Td>
-                    <Td>
-                      <HStack>
-                        <Tag colorScheme='blue'>
-                          <TagLabel>SWE</TagLabel>
-                        </Tag>
-
-                        <Tag>
-                          <TagLabel>CORE</TagLabel>
-                        </Tag>
-                      </HStack>
-                    </Td>
-                  </Tr>
-
-                  <Tr>
-                    <Td width='60%'>CS5224 Cloud Computing</Td>
-                    <Td width='10%'>4</Td>
-                    <Td>
-                      <HStack>
-                        <Tag colorScheme='red'>
-                          <TagLabel>NETWORKING</TagLabel>
-                        </Tag>
-                      </HStack>
-                    </Td>
-                  </Tr>
-                </Tbody>
-              </Table>
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <AccordionButton>
-              <Flex width='100%'>
-                <Heading size='md'>Year 1 Semester 2</Heading>
-                <Spacer />
-                <AccordionIcon />
-              </Flex>
-            </AccordionButton>
-
-            <AccordionPanel padding='0'>
-              <Table>
-                <Thead>
-                  <Tr>
-                    <Th>Module</Th>
-                    <Th>Credits</Th>
-                    <Th>Tags</Th>
-                  </Tr>
-                </Thead>
-
-                <Tbody>
-                  <Tr>
-                    <Td width='60%'>CS4225 Big Data Systems for Data Science</Td>
-                    <Td width='10%'>4</Td>
-                    <Td>
-                      <HStack>
-                        <Tag colorScheme='green'>
-                          <TagLabel>DATABASES</TagLabel>
-                        </Tag>
-                      </HStack>
-                    </Td>
-                  </Tr>
-                </Tbody>
-              </Table>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
+      <Plan sems={sems}/>
     </Section>
   )
 }
