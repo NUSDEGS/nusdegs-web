9963b43daee2712c7d960396c07139da097e1d84
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-21 18:51:38 +0800

    refactor Plan using ModuleList component

--- src/view/components/Plan.tsx
+++ src/view/components/Plan.tsx
@@ -1,11 +1,5 @@
 import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Flex, Heading, HStack, Spacer, Table, Tag, TagLabel, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-
-export interface Module {
-  code: string
-  title: string
-  mcs: number
-  tags: string[]
-}
+import ModuleList, { Module } from "./ModuleList";
 
 export interface Semester {
   seqNum: number
@@ -36,37 +30,7 @@ export default function Plan(props: PlanProps) {
             </AccordionButton>
 
             <AccordionPanel padding='0'>
-              <Table>
-                <Thead>
-                  <Tr>
-                    <Th>Module</Th>
-                    <Th>Credits</Th>
-                    <Th>Tags</Th>
-                  </Tr>
-                </Thead>
-
-                <Tbody>
-                  {
-                    sem.modules.map(module => (
-                      <Tr>
-                        <Td width='60%'>{`${module.code} ${module.title}`}</Td>
-                        <Td width='10%'>{module.mcs}</Td>
-                        <Td>
-                          <HStack>
-                            {
-                              module.tags.map(tag => (
-                                <Tag>
-                                  <TagLabel>{tag.toUpperCase()}</TagLabel>
-                                </Tag>
-                              ))
-                            }
-                          </HStack>
-                        </Td>
-                      </Tr>
-                    ))
-                  }
-                </Tbody>
-              </Table>
+              <ModuleList modules={sem.modules} />
             </AccordionPanel>
           </AccordionItem>
         ))
