7776e97efc65b5e8b23dcac3c5259dd55a3d25f2
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-26 02:32:49 +0800

    adjust whitespace

--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -10,77 +10,76 @@ import { useForm, Controller, useWatch } from 'react-hook-form'
 export default function Generate() {
   const { control } = useForm()
   const majorWatch = useWatch({ control, name: 'major' })
-
   const [ currentSectionIndex, setCurrentSectionIndex ] = useState(0)
 
   return (
-  <ChakraProvider>
-    <Head>
-      <title>NUSDegs</title>
-    </Head>
+    <ChakraProvider>
+      <Head>
+        <title>NUSDegs</title>
+      </Head>
 
-    <main>
-      <form>
-        <Section
-          title='Major'
-          description='Choose your major.'
-          hidden={currentSectionIndex !== 0}
-        >
-          <Controller
-            name='major'
-            control={control}
-            render={({field}) => (
-              <ImageRadio
-                {...field}
-                image='https://www.svgrepo.com/show/12668/computer.svg'
-                label='Computer Science'
-                value='computerScience'
-              />
-            )}
-          />
-        </Section>
-
-        <Section
-          title='All Good?'
-          description='Check your choices.'
-          hidden={currentSectionIndex !== 1}
-        >
-          <HStack>
-              <VStack align='end'>
-                <Text as='b'>Major</Text>
-                <Text as='b'>Focus Areas</Text>
-              </VStack>
-
-              <Divider orientation='vertical' />
-
-              <VStack align='start'>
-                <Text>{majorWatch}</Text>
-                <Text>Networking, and Databases</Text>
-              </VStack>
-            </HStack>
-        </Section>
-
-        <Section
-          title='Plans'
-          description='Here are your recommended plans.'
-          hidden={currentSectionIndex !== 2}
-        >
-          <Plan sems={[]} />
-        </Section>
-
-        <Center>
-          <Button
-            colorScheme='green'
-            onClick={() => setCurrentSectionIndex(currentSectionIndex + 1)}
-            isDisabled={currentSectionIndex === 2}
+      <main>
+        <form>
+          <Section
+            title='Major'
+            description='Choose your major.'
+            hidden={currentSectionIndex !== 0}
           >
-            {
-              currentSectionIndex < 1 ? 'Next' : 'Done'
-            }
-          </Button>
-        </Center>
-      </form>
-    </main>
-  </ChakraProvider>
+            <Controller
+              name='major'
+              control={control}
+              render={({field}) => (
+                <ImageRadio
+                  {...field}
+                  image='https://www.svgrepo.com/show/12668/computer.svg'
+                  label='Computer Science'
+                  value='computerScience'
+                />
+              )}
+            />
+          </Section>
+
+          <Section
+            title='All Good?'
+            description='Check your choices.'
+            hidden={currentSectionIndex !== 1}
+          >
+            <HStack>
+                <VStack align='end'>
+                  <Text as='b'>Major</Text>
+                  <Text as='b'>Focus Areas</Text>
+                </VStack>
+
+                <Divider orientation='vertical' />
+
+                <VStack align='start'>
+                  <Text>{majorWatch}</Text>
+                  <Text>Networking, and Databases</Text>
+                </VStack>
+              </HStack>
+          </Section>
+
+          <Section
+            title='Plans'
+            description='Here are your recommended plans.'
+            hidden={currentSectionIndex !== 2}
+          >
+            <Plan sems={[]} />
+          </Section>
+
+          <Center>
+            <Button
+              colorScheme='green'
+              onClick={() => setCurrentSectionIndex(currentSectionIndex + 1)}
+              isDisabled={currentSectionIndex === 2}
+            >
+              {
+                currentSectionIndex < 1 ? 'Next' : 'Done'
+              }
+            </Button>
+          </Center>
+        </form>
+      </main>
+    </ChakraProvider>
   )
 }
