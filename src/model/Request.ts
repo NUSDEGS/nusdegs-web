a0f613c27bd787c480bc3bcbbb197382ac8eeded
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-26 03:08:13 +0800

    add getRequestJson(...) inside Request.ts

--- src/model/Request.ts
+++ src/model/Request.ts
@@ -2,3 +2,10 @@ export default interface Request {
   id: number
   major: string
 }
+
+export function getRequestJson(requestFormData: any): Request {
+  return {
+    id: 0,  // TODO needs to be changed for each request.
+    major: requestFormData['major'] ?? 'computerScience'  // Computer Science is chosen by default.
+  }
+}
