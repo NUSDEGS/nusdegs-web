8486d601254b90490f592ff20b2b86b1bfc7c7cf
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2023-03-26 23:50:56 +0800

    add components/ImageCheckbox.tsx

---
+++ src/view/components/ImageCheckbox.tsx
@@ -0,0 +1,32 @@
+import { Box, VStack, Text, Image, UseCheckboxProps, useCheckbox } from "@chakra-ui/react";
+
+interface ImageCheckboxProps extends UseCheckboxProps {
+  image: string
+  label: string
+}
+
+export default function ImageCheckbox(props: ImageCheckboxProps) {
+  // Destructure props to get checkboxProps without image and label.
+  // This is to pass only checkboxProps to the useCheckbox hook.
+  const {image, label, ...checkboxProps} = props
+  const {
+    state,
+    getInputProps,
+    getCheckboxProps,
+    getLabelProps
+  } = useCheckbox(checkboxProps)
+
+  return (
+    <Box as='label'>
+      <VStack cursor='pointer'>
+        <input hidden {...getInputProps()} />
+        
+        <Box boxSize='8rem' {...getCheckboxProps()}>
+          <Image src={image} {...getLabelProps()}/>
+        </Box>
+
+        <Text as='b' color={state.isChecked ? 'green.500' : ''}>{label}</Text>
+      </VStack>
+    </Box>
+  )
+}
